a0706d7231aecd50626564c95824f88ef473754e
mostafa <Moustafa_20225056@fci.helwan.edu.eg>
2025-08-09 03:16:51 +0300

    add constant

---
+++ lib/util/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kOrderName = Color(0xff6466F1);
+const Color kOrderCompleted = Color(0xff1FA650);
+const Color kOrderNotStarted = Color(0xffD6A440);
+const Color kOrderOverDue = Color(0xffE65A58);
+const Color kDescriptionColor = Color(0xff4B5563);
